2763ddb2f1a65cd9ebd1dee63d7c2dd32a6a3443
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-03-16 12:16:58 +0200

    com11
    +MODAL

---
+++ js/swiper-init.js
@@ -0,0 +1,9 @@
+  var swiper = new Swiper(".swiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">'  + "</span>";
+          },
+        },
+      });
